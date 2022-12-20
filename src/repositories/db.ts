e6bf3870d331481a476a39b50cb9a50d4a08f9ed
Renat <jeingo20@gmail.com>
2022-12-20 20:41:24 +0300

    add mongo url and wrote runDb function

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -1,5 +1,21 @@
+import {MongoClient} from "mongodb"
 import * as dotenv from 'dotenv'
 dotenv.config()
+
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017'
+
+export const client = new MongoClient(mongoUrl)
+
+export const runDb = async () => {
+    try {
+        await client.connect()
+        await client.db('test').command({ping: 1})
+        console.log('Connected successfully to mongo db')
+    } catch {
+        console.log(`Can't connect to mongo db`)
+        await client.close()
+    }
+}
 
 export type BlogsType = {
     id: string
@@ -37,5 +53,3 @@ export const db:{blogs: Array<BlogsType>, posts: Array<PostsType>} = {
         }
     ]
 }
-
-export const runDb = async () => {}
