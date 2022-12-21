a177d026f5e9034cf20fb0132d1cc734b2e0da3f
Renat <jeingo20@gmail.com>
2022-12-21 21:05:17 +0300

    fix2

--- __tests__/e2e/posts.api.test.ts
+++ __tests__/e2e/posts.api.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest'
-import {app} from '../../src/app'
+import {app} from '../../src/index'
 import {HTTP_STATUSES} from '../../src/constats/status'
 
 const correctBlog = {
